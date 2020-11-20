647529629c223423ef35bf2dbe2c81848e741f47
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-11-20 19:39:39 +0300

    chore: add project files for selenium web driver tests

---
+++ Lab_6/SeleniumWebDriverTests/SeleniumWebDriverTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumWebDriverTests
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
