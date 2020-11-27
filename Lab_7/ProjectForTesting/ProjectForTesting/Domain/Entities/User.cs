2c681fb8362bef96f9f8fe173a4baad2d5090272
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-11-27 20:32:01 +0300

    feat: add domain model

---
+++ Lab_7/ProjectForTesting/ProjectForTesting/Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectForTesting.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
