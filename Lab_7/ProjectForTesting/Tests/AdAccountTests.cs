cee74ba86c51220baeee3fe046918e7e75c56c52
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-11-29 17:16:03 +0300

    feat: implement tests for AdAccount and UsersController

---
+++ Lab_7/ProjectForTesting/Tests/AdAccountTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ProjectForTesting.Domain.Exceptions;
+using ProjectForTesting.Domain.ValueObjects;
+
+namespace Tests
+{
+    internal class AdAccountTests
+    {
+        [Test]
+        public void ShouldHaveCorrectDomainAndName()
+        {
+            const string accountString = "MyCompany\\Shulga";
+
+            var account = AdAccount.For(accountString);
+
+            account.Domain.Should().Be("MyCompany");
+            account.Name.Should().Be("Shulga");
+        }
+
+        [Test]
+        public void ToStringReturnsCorrectFormat()
+        {
+            const string accountString = "MyCompany\\Shulga";
+
+            var account = AdAccount.For(accountString);
+
+            string result = account.ToString();
+
+            result.Should().Be(accountString);
+        }
+
+        [Test]
+        public void ImplicitConversionToStringResultsInCorrectString()
+        {
+            const string accountString = "MyCompany\\Shulga";
+
+            var account = AdAccount.For(accountString);
+
+            string result = account;
+
+            result.Should().Be(accountString);
+        }
+
+        [Test]
+        public void ExplicitConversionFromStringSetsDomainAndName()
+        {
+            const string accountString = "MyCompany\\Shulga";
+
+            var account = (AdAccount)accountString;
+
+            account.Domain.Should().Be("MyCompany");
+            account.Name.Should().Be("Shulga");
+        }
+
+        [Test]
+        public void ShouldThrowAdAccountInvalidExceptionForInvalidAdAccount()
+        {
+            FluentActions.Invoking(() => (AdAccount)"MyCompanyShulga")
+                .Should().Throw<AdAccountInvalidException>();
+        }
+    }
+}
---
+++ Lab_7/ProjectForTesting/Tests/UsersControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using Moq;
+using NUnit.Framework;
+using ProjectForTesting.Controllers;
+using ProjectForTesting.DataAccess.Repositories;
+using ProjectForTesting.Domain.Entities;
+using ProjectForTesting.Dtos;
+using ProjectForTesting.LoggerService;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class UsersControllerTests
+    {
+        [Test]
+        public async Task GetUserById()
+        {
+            var repository = new Mock<IRepository<User>>();
+            var logger = new Mock<ILoggerManager>();
+
+            var controller = new UsersController(repository.Object, logger.Object);
+            await controller.GetById(1);
+
+            repository.Verify(u => u.GetByIdAsync(1));
+        }
+
+        [Test]
+        public async Task GetAllUsers()
+        {
+            var repository = new Mock<IRepository<User>>();
+            var logger = new Mock<ILoggerManager>();
+
+            var controller = new UsersController(repository.Object, logger.Object);
+            await controller.GetAll();
+
+            repository.Verify(u => u.GetAllAsync());
+        }
+        
+        [Test]
+        public async Task AddUser()
+        {
+            var repository = new Mock<IRepository<User>>();
+            var logger = new Mock<ILoggerManager>();
+            var userDto = new UserDto
+            {
+                Firstname = "Test",
+                Surname = "Test",
+                Account = "Test\\\\Test"
+            };
+
+            var controller = new UsersController(repository.Object, logger.Object);
+            await controller.Add(userDto);
+
+            repository.Verify(u => u.AddAsync(It.IsAny<User>()));
+        }
+
+        [Test]
+        public async Task UpdateUser()
+        {
+            var repository = new Mock<IRepository<User>>();
+            var logger = new Mock<ILoggerManager>();
+            var userDto = new UserDto
+            {
+                Firstname = "Test",
+                Surname = "Test",
+                Account = "Test\\Test"
+            };
+
+            var controller = new UsersController(repository.Object, logger.Object);
+            await controller.Update(It.IsAny<int>(), userDto);
+
+            repository.Verify(u => u.AddAsync(It.IsAny<User>()));
+        }
+
+        [Test]
+        public async Task DeleteUser()
+        {
+            var repository = new Mock<IRepository<User>>();
+            var logger = new Mock<ILoggerManager>();
+
+            var controller = new UsersController(repository.Object, logger.Object);
+            await controller.Delete(1);
+
+            repository.Verify(u => u.DeleteByIdAsync(1));
+        }
+    }
+}
